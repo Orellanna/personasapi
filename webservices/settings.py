2b749b1903c54706acea0363f035f1fb361caf74
Alexis Orellana <od18003@ues.edu.sv>
2022-03-04 22:40:58 -0600

    productosAPI

--- webservices/settings.py
+++ webservices/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yp$v^e6kwi1k1bm!kbvpafbi34vzjr-i5$52q_2h__z7*@ilxf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['personasappsv.herokuapp.com']
 
 
 # Application definition
